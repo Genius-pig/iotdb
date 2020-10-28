7819502c91fb0c3b1f1a7d861e394d09045d508e
Boris <969032536@qq.com>
2020-10-28 16:36:31 +0800

    combine array in promise.

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -13,7 +13,6 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   username: string;
   password: string;
   url: string;
-  data: any[] = [];
 
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
@@ -24,38 +23,25 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const { range } = options;
-    options.targets.map(target => {
-      console.log(target);
-      console.log(range);
+    const dataFrames = options.targets.map(target => {
       target.from = range!.from.valueOf();
       target.to = range!.to.valueOf();
-      this.doRequest(target);
+      return this.doRequest(target);
     });
-
-    const dataFrames = this.data.map(toDataFrame);
-    // const { range } = options;
-    // const timeValues = [range!.from.valueOf(), range!.to.valueOf()];
-    // const numberValues = [12.3, 28.6];
-    //
-    // Create data frame from values.
-    // const frame = toDataFrame({
-    //   name: 'http_requests_total',
-    //   fields: [
-    //     { name: 'Time', type: FieldType.time, values: timeValues },
-    //     { name: 'Value', type: FieldType.number, values: numberValues },
-    //   ],
-    // });
-    return Promise.all(dataFrames).then(data => ({ data }));
+    return Promise.all(dataFrames)
+      .then(arrays => [].concat.apply([], arrays))
+      .then(data => ({ data }));
   }
 
   async doRequest(query: MyQuery) {
-    await getBackendSrv()
+    return await getBackendSrv()
       .datasourceRequest({
         method: 'POST',
         url: this.url + '/query',
         data: query,
       })
-      .then(response => (this.data = response.data));
+      .then(response => response.data)
+      .then(a => a.map(toDataFrame));
   }
 
   async testDatasource() {
