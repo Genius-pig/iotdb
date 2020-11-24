88c50617faa2a87c73c34a707d2c3688b7b16a56
Boris <969032536@qq.com>
2020-11-24 18:07:51 +0800

    change url

--- src/functions.ts
+++ src/functions.ts
@@ -4,10 +4,11 @@ import { getBackendSrv } from '@grafana/runtime';
 export const toOption = (value: string) => ({ label: value, value } as SelectableValue<string>);
 
 export async function getChildPaths(detachedPath: string[], url: string) {
+  const prefixPath: string = detachedPath.reduce((a, b) => a + '.' + b);
   return await getBackendSrv()
     .datasourceRequest({
-      method: 'POST',
-      url: url + '/getChildPath',
+      method: 'GET',
+      url: url + '/getChildPaths?path=' + prefixPath,
     })
     .then(response => response.data)
     .then(data => data.map(toOption));
